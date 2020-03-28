51e599988fd7f140bfeb36feb88f964bb71a9f10
mingochen <mingochen@tencent.com>
2020-03-28 09:55:31 +0800

    [feature] django restful框架搭建

---
+++ api/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render
+from django.contrib.auth.models import User,Group
+from rest_framework import viewsets
+from api.serializers import UserSerializer,GroupSerializer
+# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
